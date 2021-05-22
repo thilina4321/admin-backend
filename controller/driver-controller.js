bc2b990417dff36156c881e38bc918397376075d
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-05-22 20:23:25 +0530

    better

--- controller/driver-controller.js
+++ controller/driver-controller.js
@@ -41,9 +41,8 @@ exports.addProfileImage = async(req,res)=>{
 
     try {
 
-        const image = await cloudinary.uploader.upload(proImage)
         const img = await Driver.findOneAndUpdate({userId:id},
-           {profileImage:image.url}, {new:true})
+           {profileImage:proImage}, {new:true})
         res.status(200).send({img})
     } catch (error) {
         res.status(500).send({error:proImage})
